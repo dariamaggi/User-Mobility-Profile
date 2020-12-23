a96d9f10532aa7646f5d88e18576c7e48faa3c1b
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-12-23 18:26:42 +0100

    added user list

--- UserMobilityProfileManagerModule/interface/myApp.py
+++ UserMobilityProfileManagerModule/interface/myApp.py
@@ -11,32 +11,49 @@ class Example(Frame):
         super().__init__()
 
         self.initUI()
-        self.buildFrames()
 
 
     def buildFrames(self):
 
-        labelframe = LabelFrame(self.master)
-        labelframe.pack(fill="both", expand="yes")
+        self.labelframe = LabelFrame(self.master, text= "Currently Loaded User Mobility Profiles")
+        self.labelframe.pack(fill="both", expand="yes")
 
-        left = Label(labelframe)
+        left = Label(self.labelframe)
         left.pack()
 
-        labelframe1 = LabelFrame(self.master)
+        labelframe1 = LabelFrame(self.master, text= "Console Log")
+
         labelframe1.pack(fill="both", expand="yes")
-        right = Label(labelframe)
+        scrollbary = Scrollbar(self.labelframe)
+        scrollbarx = Scrollbar(self.labelframe, orient=HORIZONTAL)
+
+        listbox = Listbox(self.labelframe, yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+        listbox.insert(END, "Riccardo Bertini")
+        listbox.insert(END, "Ajeje Brazorf")
+
+
+        scrollbary.config(command=listbox.yview)
+        scrollbarx.config(command=listbox.xview)
+        scrollbarx.pack(side=BOTTOM, fill=X)
+
+        listbox.pack(side=LEFT, fill=BOTH)
+        scrollbary.pack(side=RIGHT, fill=Y)
+
+        right = Label(self.labelframe)
         right.pack()
 
-    def initUI(self):
-
-        self.master.title("User Mobility Profile")
-
+    def setMenu(self):
         menubar = Menu(self.master)
         self.master.config(menu=menubar)
 
         fileMenu = Menu(menubar)
         fileMenu.add_command(label="Exit", command=self.onExit)
         menubar.add_cascade(label="File", menu=fileMenu)
+    def initUI(self):
+
+        self.master.title("User Mobility Profile")
+        self.buildFrames()
+
 
 
     def onExit(self):
