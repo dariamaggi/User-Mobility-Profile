a96d0b0891f2718dff911adac8eba3eb3f284c79
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-12-24 13:49:30 +0100

    Update myApp.py

--- UserMobilityProfileManagerModule/src/interface/myApp.py
+++ UserMobilityProfileManagerModule/src/interface/myApp.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 import datetime
-
+from tkinter.messagebox import showinfo
 class Example(Frame):
 
     def __init__(self):
@@ -14,13 +14,10 @@ class Example(Frame):
 
         self.labelframe = LabelFrame(self.master, text= "Currently Loaded User Mobility Profiles")
         self.labelframe.pack(fill="both", expand="yes")
-
         left = Label(self.labelframe)
         left.pack()
 
         self.labelframe1 = LabelFrame(self.master, text= "Console Log")
-
-
         self.labelframe1.pack(fill="both", expand="yes")
 
         vsb = Scrollbar(self.labelframe, orient="vertical")
@@ -43,7 +40,7 @@ class Example(Frame):
         self.listbox.pack(expand="yes", fill=BOTH)
 
         self.listbox.bind('<<ListboxSelect>>', self.onselect)
-        right = Label(self.labelframe)
+        right = Label(self.labelframe1)
         right.pack()
 
 
@@ -57,18 +54,34 @@ class Example(Frame):
         value = w.get(index)
         self.listbox_insert(value)
 
-    def setMenu(self):
-        menubar = Menu(self.master)
-        self.master.config(menu=menubar)
-
-        fileMenu = Menu(menubar)
-        fileMenu.add_command(label="Exit", command=self.onExit)
-        menubar.add_cascade(label="File", menu=fileMenu)
     def initUI(self):
 
         self.master.title("User Mobility Profile")
         self.buildFrames()
+        self.buildMenu()
 
+    def about(self):
+        showinfo("About","This project was made for the 2020/2021 edition of the Industrial Application course at the University of Pisa.")
+        self.master.update()
+    def addUser(self):
+        showinfo("Add user","This has not been implemented yet.")
+
+    def buildMenu(self):
+
+        self.master.title("Simple Menu")
+        menubar = Menu(self.master)
+        self.master.config(menu = menubar)
+
+        options= Menu(menubar)
+        options.add_command(label = "Add user", command = self.addUser)
+
+        menubar.add_cascade(label = "Options", menu = options)
+
+        filemenu = Menu(menubar)
+        filemenu.add_command(label = "Exit", command = self.onExit)
+        filemenu.add_command(label = "About", command = self.about)
+
+        menubar.add_cascade(label = "Help", menu = filemenu)
 
 
     def onExit(self):
