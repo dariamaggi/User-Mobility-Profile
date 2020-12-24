e0d8f41dee7a3c4a18c6552cfcc659519c8c5c6b
Federico <lapennafederico@gmail.com>
2020-12-24 18:12:15 +0100

    Add base class for test db (test_DatabaseConnector.py)

---
+++ UserMobilityProfileManagerModule/tests/test_DatabaseConnector.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_rundb(self):
+        self.run()
+
+
+class Test(TestCase):
+    def test_populate_db(self):
+        self.run()
