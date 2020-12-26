a759b0522aca1744df2dce4734bae873efc1d7ae
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-12-26 10:58:35 +0100

    Started refactoring of interface reg form

--- UserMobilityProfileManagerModule/src/interface/myApp.py
+++ UserMobilityProfileManagerModule/src/interface/myApp.py
@@ -4,12 +4,20 @@ import datetime
 from tkinter.messagebox import showinfo
 
 
+class UserProfile(Frame):
+    def __init__(self, value):
+        super().__init__()
+
+        self.initUI(value)
+
+    def initUI(self, info):
+        print(info)
 
 class MainWindow(Frame):
 
     def __init__(self):
         super().__init__()
-
+        self.users=[]
         self.initUI()
 
 
@@ -51,16 +59,13 @@ class MainWindow(Frame):
         self.listbox1.insert(END, str(datetime.datetime.now())+":"+"You opened user profile: "+value)
 
     def onselect(self, evt):
-
         w = evt.widget
         index = int(w.curselection()[0])
         value = w.get(index)
         self.listbox_insert(value)
         self.create_window(value)
 
-
     def initUI(self):
-
         self.master.title("User Mobility Profile")
         self.buildFrames()
         self.buildMenu()
@@ -68,41 +73,75 @@ class MainWindow(Frame):
     def about(self):
         showinfo("About","This project was made for the 2020/2021 edition of the Industrial Application course at the University of Pisa.")
         self.master.update()
-    def addUser(self):
-        showinfo("Add user","This has not been implemented yet.")
+    def readFromUMP(self, user_id, field):
+        print("TODO")
+    def writefromUMP(self):
+        print("TODO")
+
+
 
     def buildMenu(self):
-
         menubar = Menu(self.master)
         self.master.config(menu = menubar)
 
         options= Menu(menubar)
         options.add_command(label = "Add user", command = self.addUser)
-
         menubar.add_cascade(label = "Options", menu = options)
 
         filemenu = Menu(menubar)
         filemenu.add_command(label = "Exit", command = self.onExit)
         filemenu.add_command(label = "About", command = self.about)
-
         menubar.add_cascade(label = "Help", menu = filemenu)
 
 
     def onExit(self):
-
         self.quit()
 
     def create_window(self, value):
-        self.counter = 1
         t = Toplevel(self)
         t.wm_title("User Mobility Profile - "+ value)
         t.geometry("560x560+350+300")
+    def addUser(self):
+
+        t = Toplevel(self)
+        t.wm_title("Registration Form")
+        t.geometry("560x560+350+300")
+
+        lbl_username = Label(t, text="Name:", bd=18)
+        lbl_username.grid(row=1, column=1)
+        lbl_password = Label(t, text="Surname:", bd=18)
+        lbl_password.grid(row=2,column=1)
+        var = IntVar()
+        lbl_gender= Label(t, text="Gender", bd=18)
+        lbl_gender.grid(row=8, column=1)
+        rd= Radiobutton(t, text="Male", padx=5, variable=var, value=1)
+        rd.grid(row=8, column=2)
+        rd1 = Radiobutton(t, text="Female", padx=5, variable=var, value=1)
+        rd1.grid(row=8, column=3)
+        lbl_firstname = Label(t, text="Gender:", font=('arial', 18), bd=18)
+        lbl_firstname.grid(row=3,column=1)
+        lbl_lastname = Label(t, text="Address:", font=('arial', 18), bd=18)
+        lbl_lastname.grid(row=4,column=1)
+        lbl_result = Label(t, text=" DDD", font=('lato', 18))
+        lbl_result.grid(row=5,column=1, columnspan=4)
 
 
-#      l = Label(t, text="This is window #%s" % self.counter)
-  #      l.pack(side="top", fill="both", expand=True, padx=100, pady=100)
+
+        user = Entry(t, font=('lato', 20), width=15)
+        user.grid(row=1, column=2)
+        pass1 = Entry(t, font=('lato', 20),  width=15, show="*")
+        pass1.grid(row=2, column=2)
+        name = Entry(t, font=('lato', 20), width=15)
+        name.grid(row=3, column=2)
+        address = Entry(t, font=('lato', 20), width=15)
+        address.grid(row=4, column=2)
 
 
+        btn_register = Button(t, font=('lato', 20), text="Register")
+        btn_register.grid(row=6, column=2, columnspan=2)
+
+        # this will run the mainloop.
+        t.mainloop()
 
 def main():
 
