328cf454d15ed2a14b4514781d1e98abb8619c60
Federico <lapennafederico@gmail.com>
2020-12-28 17:18:53 +0100

    add class UserIdentificationLogic.py

---
+++ UserMobilityProfileManagerModule/src/UserIdentificationLogic.py
@@ -0,0 +1,7 @@
+# TODO: Marsha and Andrea
+
+
+def identify_user(data):
+    return data   # todo: deve chiamare la funzione per il riconoscimento e restiruire
+    #                 1 --> non trovato
+    #                 id dell'utente
