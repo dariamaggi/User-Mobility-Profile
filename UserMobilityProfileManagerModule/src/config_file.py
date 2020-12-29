b7ac58eccde4909a0579ed0c3bbb7e7397dc9400
Federico <lapennafederico@gmail.com>
2020-12-29 18:23:15 +0100

    change config_file.py

--- UserMobilityProfileManagerModule/src/config_file.py
+++ UserMobilityProfileManagerModule/src/config_file.py
@@ -2,6 +2,7 @@ import configparser
 
 config = configparser.ConfigParser()
 config['settings'] = {
-    'img_path': 'files/photos'}
+    'img_path': 'files/photos',
+    'mongo_con': 'localhost'}
 with open('../files/configurations.ini', 'w') as configfile:
     config.write(configfile)
